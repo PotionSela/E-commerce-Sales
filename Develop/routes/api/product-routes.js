8854fe65646e154cde441a5684088f54885c39e3
Gisela <giselamata27@gmail.com>
2024-01-09 14:53:16 -0500

    Added in delete route

--- Develop/routes/api/product-routes.js
+++ Develop/routes/api/product-routes.js
@@ -43,7 +43,7 @@ router.get('/:id', async (req, res) => {
 });
 
 // create new product
-router.post('/', (req, res) => {
+router.post('/', async (req, res) => {
   /* req.body should look like this...
     {
       product_name: "Basketball",
@@ -119,8 +119,22 @@ router.put('/:id', (req, res) => {
     });
 });
 
-router.delete('/:id', (req, res) => {
+router.delete('/:id', async (req, res) => {
   // delete one product by its `id` value
+try {
+  const products = await Product.destory({
+    where: {
+      id: req.params.id,
+    },
+  });
+  if (!products) {
+    res.status(404).json({ message: 'No product found with that id'});
+    return;
+  }
+  res.status(200).json(products);
+} catch (err) {
+  res.status(500).json(err);
+}
 });
 
 module.exports = router;
